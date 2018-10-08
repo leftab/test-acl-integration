9a043ad634bbc2e0c92594ae120796dc95c8916e
Maxime Cote <maxime.cote184@gmail.com>
2018-10-08 10:32:08 -0400

    Add datastore contract functions

--- contracts/CounterApp.sol
+++ contracts/CounterApp.sol
@@ -22,7 +22,11 @@ contract CounterApp is AragonApp {
 
     function increment(uint256 step) auth(INCREMENT_ROLE) external auth(DATASTORE_MANAGER_ROLE)  {
         value = value.add(step);
-        datastoreACL.test();
+        uint fId = fileList.addFile("", "", 2, true);
+
+        PermissionLibrary.addOwner(permissions, fId, msg.sender);
+        PermissionLibrary.initializePermissionAddresses(permissions, fId);
+        emit NewFile(msg.sender);
     }
 
     function decrement(uint256 step) auth(DECREMENT_ROLE) external {
@@ -86,6 +90,11 @@ contract CounterApp is AragonApp {
     ACL private acl;
     DatastoreACL private datastoreACL;
 
+    modifier onlyFileOwner(uint256 _fileId) {
+        require(permissions.isOwner(_fileId, msg.sender));
+        _;
+    }    
+
     function initialize(address _datastoreACL) onlyInit public {
         initialized();
 
@@ -95,6 +104,370 @@ contract CounterApp is AragonApp {
         permissions.init(datastoreACL);
         groups.init(datastoreACL);              
     }
+
+    /**
+     * @notice Add a file to the datastore
+     * @param _storageRef Storage Id of the file (IPFS only for now)
+     * @param _name File name
+     * @param _fileSize File size in bytes
+     * @param _isPublic Is file readable by anyone
+     */
+    function addFile(string _storageRef, string _name, uint _fileSize, bool _isPublic) 
+        external 
+        auth(DATASTORE_MANAGER_ROLE) 
+        returns (uint fileId) 
+    {
+        uint fId = fileList.addFile(_storageRef, _name, _fileSize, _isPublic);
+
+        PermissionLibrary.addOwner(permissions, fId, msg.sender);
+        PermissionLibrary.initializePermissionAddresses(permissions, fId);
+        emit NewFile(msg.sender);
+        return fId;
+    }
+
+    /**
+     * @notice Returns the file with Id `_fileId`
+     * @param _fileId File id
+     */
+    function getFile(uint _fileId) 
+        external
+        view 
+        returns (
+            string storageRef,
+            string name,
+            uint fileSize,
+            bool isPublic,
+            bool isDeleted,
+            address owner,
+            bool isOwner,
+            uint lastModification,
+            address[] permissionAddresses,
+            bool writeAccess
+        ) 
+    {
+        FileLibrary.File storage file = fileList.files[_fileId];
+
+        storageRef = file.storageRef;
+        name = file.name;
+        fileSize = file.fileSize;
+        isPublic = file.isPublic;
+        isDeleted = file.isDeleted;
+        owner = permissions.fileOwners[_fileId];
+        isOwner = permissions.isOwner(_fileId, msg.sender);
+        lastModification = file.lastModification;
+        permissionAddresses = permissions.permissionAddresses[_fileId];
+        writeAccess = hasWriteAccess(_fileId, msg.sender);
+    }
+
+
+
+    /**
+     * @notice Delete file with Id `_fileId`
+     * @param _fileId File Id
+     */
+    function deleteFile(uint _fileId) public onlyFileOwner(_fileId) {
+        fileList.deleteFile(_fileId);
+    }
+
+    function lastFileId() external view returns (uint256) {
+        return fileList.lastFileId;
+    }
+
+    /**
+     * @notice Changes name of file `_fileId` to `_newName`
+     * @param _fileId File Id
+     * @param _newName New file name
+     */
+    function setFileName(uint _fileId, string _newName) external {
+        require(hasWriteAccess(_fileId, msg.sender));
+
+        fileList.setFileName(_fileId, _newName);
+    }
+
+    /**
+     * @notice Change file content of file `_fileId` to content stored at `_storageRef`
+     * with size of `_fileSize` bytes
+     * @param _fileId File Id
+     * @param _storageRef Storage Id (IPFS)
+     * @param _fileSize File size in bytes
+     */
+    function setFileContent(uint _fileId, string _storageRef, uint _fileSize) external {
+        require(hasWriteAccess(_fileId, msg.sender));
+
+        fileList.setFileContent(_fileId, _storageRef, _fileSize);
+    }
+
+    /**
+     * @notice Returns entity addresses on which permissions are set for file `_fileId`
+     * @param _fileId File Id
+     * @return addresses Array of entity addresses
+     */
+    function getEntitiesWithPermissionsOnFile(uint _fileId) 
+        external 
+        view 
+        returns (address[]) 
+    {
+        return permissions.permissionAddresses[_fileId];
+    }
+
+    /**
+     * @notice Returns group ids on which permissions are set for file `_fileId`
+     * @param _fileId File Id
+     * @return Array of group ids
+     */
+    function getGroupsWithPermissionsOnFile(uint _fileId) 
+        external 
+        view 
+        returns (uint256[]) 
+    {
+        return permissions.groupIds[_fileId];
+    }
+
+    /**
+     * @notice Get write and read permissions for entity `_entity` on file `_fileId`
+     * @param _fileId File Id
+     * @param _entity Entity address
+     */
+    function getEntityPermissionsOnFile(uint256 _fileId, address _entity) 
+        external 
+        view 
+        returns (bool write, bool read) 
+    {
+        /*
+        PermissionLibrary.Permission storage permission = permissions.entityPermissions[_fileId][_entity];
+        write = permission.write;
+        read = permission.read;*/
+        return permissions.getEntityPermissionsOnFile(_fileId, _entity);
+    } 
+
+    /**
+     * @notice Get write and read permissions for group `_groupId` on file `_fileId`
+     * @param _fileId File Id
+     * @param _groupId Group Id
+     */
+    function getGroupPermissionsOnFile(uint256 _fileId, uint256 _groupId) 
+        external 
+        view 
+        returns (bool write, bool read) 
+    {
+        PermissionLibrary.Permission storage permission = permissions.groupPermissions[_fileId][_groupId];
+        write = permission.write;
+        read = permission.read;
+    } 
+
+
+    /**
+     * @notice Add/Remove permissions to an entity for a specific file
+     * @param _fileId File Id
+     * @param _entity Entity address
+     * @param _read Read permission
+     * @param _write Write permission     
+     */
+    function setEntityPermissions(uint _fileId, address _entity, bool _read, bool _write) 
+        external 
+        onlyFileOwner(_fileId) 
+    {
+        permissions.setEntityPermissions(_fileId, _entity, _read, _write);
+        emit NewEntityPermissions(msg.sender);
+    }
+
+    /**
+     * @notice Remove entity from file permissions
+     * @param _fileId Id of the file
+     * @param _entity Entity address
+     */
+    function removeEntityFromFile(uint _fileId, address _entity) external onlyFileOwner(_fileId) {
+        permissions.removeEntityFromFile(_fileId, _entity);
+        emit EntityPermissionsRemoved(msg.sender);       
+    }
+    
+    /**
+     * @notice Change the storage provider
+     * @param _storageProvider Storage provider
+     */
+    function setStorageProvider(StorageProvider _storageProvider) public {
+        require(settings.storageProvider == StorageProvider.None);
+        settings.storageProvider = _storageProvider;
+        emit SettingsChanged(msg.sender);
+    }
+
+    /**
+     * Sets IPFS as the storage provider for the datastore.
+     * Since switching between storage providers is not supported,
+     * the method can only be called if storage isn't set or already IPFS
+     */
+    function setIpfsStorageSettings(string host, uint16 port, string protocol) public {
+        require(settings.storageProvider == StorageProvider.None || settings.storageProvider == StorageProvider.Ipfs);
+
+        settings.ipfsHost = host;
+        settings.ipfsPort = port;
+        settings.ipfsProtocol = protocol;
+        /*
+        settings.ipfs = IpfsSettings({
+            host: host,
+            port: port,
+            protocol: protocol
+        });*/
+
+        settings.storageProvider = StorageProvider.Ipfs;
+        emit SettingsChanged(msg.sender);
+    }
+
+    /**
+     * @notice Returns true if `_entity` has read access on file `_fileId`
+     * @param _fileId File Id
+     * @param _entity Entity address     
+     */
+    function hasReadAccess(uint _fileId, address _entity) public view returns (bool) {
+        if (permissions.hasReadAccess(_fileId, _entity))
+            return true;
+
+        for (uint i = 0; i < groups.groupList.length; i++) {
+            if (permissions.groupPermissions[_fileId][groups.groupList[i]].exists) {
+                if (permissions.groupPermissions[_fileId][groups.groupList[i]].read) {
+                    if (groups.isEntityInGroup(groups.groupList[i], _entity)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @notice Returns true if `_entity` has write access on file `_fileId`
+     * @param _fileId File Id
+     * @param _entity Entity address     
+     */
+    function hasWriteAccess(uint _fileId, address _entity) public view returns (bool) {
+        if (permissions.hasWriteAccess(_fileId, _entity))
+            return true;
+
+        for (uint i = 0; i < groups.groupList.length; i++) {
+            if (permissions.groupPermissions[_fileId][groups.groupList[i]].exists) {
+                if (permissions.groupPermissions[_fileId][groups.groupList[i]].write) {
+                    if (groups.isEntityInGroup(groups.groupList[i], _entity)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @notice Add a group to the datastore
+     * @param _groupName Name of the group
+     */
+    function createGroup(string _groupName) external auth(DATASTORE_MANAGER_ROLE) returns (uint) {
+        uint id = groups.createGroup(_groupName);
+        emit GroupChange(msg.sender);
+        return id;
+    }
+
+    /**
+     * @notice Delete a group from the datastore
+     * @param _groupId Id of the group to delete
+     */
+    function deleteGroup(uint _groupId) external auth(DATASTORE_MANAGER_ROLE) {
+        require(groups.groups[_groupId].exists);
+        groups.deleteGroup(_groupId);
+        emit GroupChange(msg.sender);
+    }
+
+    /**
+     * @notice Rename a group
+     * @param _groupId Id of the group to rename
+     * @param _newGroupName New name for the group
+     */
+    function renameGroup(uint _groupId, string _newGroupName) external auth(DATASTORE_MANAGER_ROLE) {
+        require(groups.groups[_groupId].exists);
+        groups.renameGroup(_groupId, _newGroupName);
+        emit GroupChange(msg.sender);
+    }
+
+    /**
+     * @notice Get a specific group
+     * @param _groupId Id of the group to return
+     */
+    function getGroup(uint _groupId) public view returns (address[], string) {
+        require(groups.groups[_groupId].exists);
+        return groups.getGroup(_groupId);
+    }
+
+    /**
+     * @notice Get a list of all the groups Id's
+     */
+    function getGroupIds() public view returns (uint[]){
+        return groups.groupList;
+    }
+
+
+    /**
+     * @notice Add an entity to a group
+     * @param _groupId Id of the group to add the entity in
+     * @param _entity Address of the entity
+     */
+    function addEntityToGroup(uint _groupId, address _entity) public {
+        require(groups.groups[_groupId].exists);
+        groups.addEntityToGroup(_groupId, _entity);
+        emit GroupChange(msg.sender);
+    }
+
+    /**
+     * @notice Remove an entity from a group
+     * @param _groupId Id of the group to remove the entity from 
+     * @param _entity Address of the entity
+     */
+    function removeEntityFromGroup(uint _groupId, address _entity) public {
+        require(groups.groups[_groupId].exists);
+        groups.removeEntityFromGroup(_groupId, _entity);
+        emit GroupChange(msg.sender);
+    }
+
+    /**
+     * @notice Set the read and write permissions on a file for a specified group
+     * @param _fileId Id of the file
+     * @param _groupId Id of the group
+     * @param _read Read permission
+     * @param _write Write permission
+     */
+    function setGroupPermissions(uint _fileId, uint _groupId, bool _read, bool _write) public onlyFileOwner(_fileId) {
+        permissions.setGroupPermissions(_fileId, _groupId, _read, _write);
+        emit NewGroupPermissions(msg.sender);
+    }
+
+    /**
+     * @notice Set the read and write permissions on a file
+     * @param _fileId Id of the file
+     * @param _groupIds Ids of the groups
+     * @param _groupRead Read permission
+     * @param _groupWrite Write permission
+     * @param _entities Ids of the groups
+     * @param _entityRead Read permission
+     * @param _entityWrite Write permission      
+     */
+    function setMultiplePermissions(uint256 _fileId, uint256[] _groupIds, bool[] _groupRead, bool[] _groupWrite, address[] _entities, bool[] _entityRead, bool[] _entityWrite, bool _isPublic) public onlyFileOwner(_fileId) {
+        
+        for(uint256 i = 0; i < _groupIds.length; i++) 
+            permissions.setGroupPermissions(_fileId, _groupIds[i], _groupRead[i], _groupWrite[i]);
+        
+        for(uint256 j = 0; j < _entities.length; j++) 
+            permissions.setEntityPermissions(_fileId, _entities[j], _entityRead[j], _entityWrite[j]);
+
+        fileList.files[_fileId].isPublic = _isPublic;
+        emit NewPermissions(msg.sender);
+    }    
+
+    /**
+     * @notice Remove group from file permissions
+     * @param _fileId Id of the file
+     * @param _groupId Id of the group
+     */
+    function removeGroupFromFile(uint _fileId, uint _groupId) public onlyFileOwner(_fileId) {
+        permissions.removeGroupFromFile(_fileId, _groupId);
+        emit GroupPermissionsRemoved(msg.sender);
+    }    
 
 
 }
